8ef336dacdd6bc21220e64eea26904b09fac1d6e
Javen <jxveninc@gmail.com>
2026-05-30 03:05:06 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,2 @@
-const CACHE_NAME = 'instaindir-v5';
-const STATIC_ASSETS = [
-  '/instaindir/',
-  '/instaindir/index.html',
-  '/instaindir/style.css',
-  '/instaindir/app.js',
-  '/instaindir/manifest.json',
-  '/instaindir/icons/icon-192.png',
-  '/instaindir/icons/icon-512.png',
-];
-
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
